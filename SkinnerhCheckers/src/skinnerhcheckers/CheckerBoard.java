f825d524e65c82c49e70a8f192d0755474203719
Holt Skinner <HoltWasHere@gmail.com>
2017-01-24 18:18:33 -0600

    Added getters and setters

--- SkinnerhCheckers/src/skinnerhcheckers/CheckerBoard.java
+++ SkinnerhCheckers/src/skinnerhcheckers/CheckerBoard.java
@@ -1,5 +1,6 @@
 package skinnerhcheckers;
 
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 
 /**
@@ -8,21 +9,63 @@ import javafx.scene.paint.Color;
  */
 public class CheckerBoard {
     
-    private int numRows = 8, numCols = 8;
-    private double boardWidth = 0, boardHeight = 0;
-    private Color lightColor = Color.RED, darkColor = Color.BLACK;
+    private int numRows, numCols;
+    private double boardWidth, boardHeight, rectangleWidth, rectangleHeight;
+    private Color lightColor, darkColor;
+    
+    private AnchorPane board;
 
     public CheckerBoard(int numRows, int numCols, double boardWidth, double boardHeight) {
+        this(numRows, numCols, boardWidth, boardHeight, Color.RED, Color.BLACK);
+    }
+    
+    public CheckerBoard(int numRows, int numCols, double boardWidth, double boardHeight, Color lightColor, Color darkColor) {
         this.numRows = numRows;
         this.numCols = numCols;
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
-    }
-    
-    public CheckerBoard(int numRows, int numCols, double boardWidth, double boardHeight, Color lightColor, Color darkColor) {
-        this(numRows, numCols, boardWidth, boardHeight);
         this.lightColor = lightColor;
         this.darkColor = darkColor;
     }
     
+    public AnchorPane build() {
+        
+        return null;
+    }
+    
+    public AnchorPane getBoard() {
+        return board != null ? board : null;
+    }
+    
+    public int getNumRows() {
+        return this.numRows;
+    }
+    
+    public int getNumCols() {
+        return this.numCols;
+    }
+    
+    public double getWidth() {
+        return this.boardWidth;
+    }
+    
+    public double getHeight() {
+        return this.boardHeight;
+    }
+    
+    public Color getLightColor() {
+        return this.lightColor;
+    }
+    
+    public Color getDarkColor() {
+        return this.darkColor;
+    }
+    
+    public double getRectangleWidth() {
+        return this.rectangleWidth;
+    }
+    
+    public double getRectangleHeight() {
+        return this.rectangleHeight;
+    }
 }
